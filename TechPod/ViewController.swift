07b664fe148201c88574bfb70a7c4de7c47753b0
Ayaka Yasunaga <yasu.1522sky@gmail.com>
2021-02-02 17:40:33 +0900

    Initial Commit

---
+++ TechPod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechPod
+//
+//  Created by Ayaka Yasunaga on 2021/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
